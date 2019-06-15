591b61e9ff46e8b77c89462b2bda559e1572d01b
hossein20s <hossein.objectj@gmail.com>
2019-06-15 12:11:22 -0700

    Update modelutil.py

--- modelutil.py
+++ modelutil.py
@@ -1,6 +1,12 @@
+import keras
 from keras.models import model_from_json
 from keras.models import model_from_yaml
 
+# Display training progress by printing a single dot for each completed epoch
+class PrintDot(keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs):
+    if epoch % 100 == 0: print('')
+    print('.', end='')
 
 def load_model_json(dir, model_name):
   return load_model(dir, model_name, '.json')
