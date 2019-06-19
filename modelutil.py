2134c4d5131673ca55ea07e577630add23c87a64
hossein20s <hossein.objectj@gmail.com>
2019-06-19 14:09:34 -0700

    Update modelutil.py

--- modelutil.py
+++ modelutil.py
@@ -133,8 +133,9 @@ def randomForestRegressorModel(X, y):
     return scores
   
 def runRandomForestRegressor(X, Y, max_depth=10, n_estimators=100):
+  print('Y.shape shouuld be (X,)', Y.shape)
   grf = RandomForestRegressor(max_depth=max_depth, random_state=0, n_estimators=n_estimators)
-  grf.fit(X, Y)
+  grf.fit(X, Y.reshape(-1,1))
 
   predicted = grf.predict(X)
   return np.mean((predicted-Y)**2)
