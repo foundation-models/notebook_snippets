ab06cccc9579b8bc7199ad2f5cdacf31d50be5a6
hossein20s <hossein.objectj@gmail.com>
2019-06-18 21:32:14 -0700

    Update modelutil.py

--- modelutil.py
+++ modelutil.py
@@ -106,6 +106,30 @@ def load_model_weights(dir, model, model_name, epoch=0):
     file_name = dir + '/model.' + model_name + ext + '.h5'
     print('loading weights from ', file_name)
     model.load_weights(file_name)
+    
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import MinMaxScaler
+
+def randomForestRegressorModel(X, y):
+# Perform Grid-Search
+    gsc = GridSearchCV(
+        estimator=RandomForestRegressor(),
+        param_grid={
+            'max_depth': range(3,7),
+            'n_estimators': (10, 50, 100, 1000),
+        },
+        cv=5, scoring='neg_mean_squared_error', verbose=0,                         n_jobs=-1)
+    
+    grid_result = gsc.fit(X, y)
+    best_params = grid_result.best_params_
+    
+    rfr = RandomForestRegressor(max_depth=best_params["max_depth"], n_estimators=best_params["n_estimators"],                               random_state=False, verbose=False)
+# Perform K-Fold CV
+   scores = cross_val_score(rfr, X, y, cv=10, scoring='neg_mean_absolute_error')
+
+    return scores
+
   
 print('save and load models from yaml and json files defined.\
  Everything stored in folder ', dir)
