15699b83ca1d486cd1721df34459c705399994b1
hossein20s <hossein.objectj@gmail.com>
2019-06-19 12:04:23 -0700

    Update modelutil.py

--- modelutil.py
+++ modelutil.py
@@ -116,7 +116,7 @@ def randomForestRegressorModel(X, y):
     gsc = GridSearchCV(
         estimator=RandomForestRegressor(),
         param_grid={
-            'max_depth': range(3,7),
+            'max_depth': range(3,7),             
             'n_estimators': (10, 50, 100, 1000),
         },
         cv=5, scoring='neg_mean_squared_error', verbose=0,                         n_jobs=-1)
@@ -124,12 +124,19 @@ def randomForestRegressorModel(X, y):
     grid_result = gsc.fit(X, y)
     best_params = grid_result.best_params_
     
-    rfr = RandomForestRegressor(max_depth=best_params["max_depth"], n_estimators=best_params["n_estimators"],                               random_state=False, verbose=False)
+    rfr = RandomForestRegressor(max_depth=best_params["max_depth"], n_estimators=best_params["n_estimators"], 
+                                random_state=False, verbose=False)
     # Perform K-Fold CV
     scores = cross_val_score(rfr, X, y, cv=10, scoring='neg_mean_absolute_error')
 
     return scores
+  
+def randomForestRegressor(X, Y, max_depth=10, n_estimators=100):
+  grf = RandomForestRegressor(max_depth=max_depth, random_state=0, n_estimators=n_estimators)
+  grf.fit(X, Y)
 
+  predicted = grf.predict(X)
+  return np.mean((predicted-Y)**2)
   
 print('save and load models from yaml and json files defined.\
  Everything stored in folder ', dir)
