c2971a722213e7bf2b7889a89bf1e201d12c713a
hossein20s <hossein.objectj@gmail.com>
2019-06-22 11:08:21 -0700

    Update datautil.py

--- datautil.py
+++ datautil.py
@@ -44,11 +44,14 @@ class data_reader():
         length0 = self.scaledDataFrame.shape[0]
         sliding_window_data = np.zeros((length0-window_size, window_size))
         sliding_window_label = np.zeros((length0-window_size, 1))
-        for counter in range(length0-window_size):
-            if(self.feature_column is not None):
+        print(self.scaledDataFrame[self.feature_column].size)
+        if(self.feature_column is not None):
+            for counter in range(length0-window_size):
                 sliding_window_data[counter, :] = self.scaledDataFrame[self.feature_column][counter: counter+window_size]
+        print('XXXXXXX')
+        for counter in range(length0-window_size):
             sliding_window_label[counter, :] = self.scaledDataFrame[self.label_column][counter+window_size]
-            print(length0, ': ', counter)
+            
         # Random shuffle
         print('xx')
         length = sliding_window_data.shape[0]
