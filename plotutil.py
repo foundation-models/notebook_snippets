0817d41d9de926f297e4fe36fb2e41d7186c448a
hossein20s <hossein.objectj@gmail.com>
2019-06-25 19:37:12 -0700

    Update plotutil.py

--- plotutil.py
+++ plotutil.py
@@ -2,7 +2,8 @@ from generic_feature_statistics_generator import GenericFeatureStatisticsGenerat
 import base64
 from IPython.core.display import display, HTML
 
-HTML_TEMPLATE = """
+def facets_display(data):
+  HTML_TEMPLATE = """
         <script src="https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/0.7.24/webcomponents-lite.js"></script>
         <link rel="import" href="https://raw.githubusercontent.com/PAIR-code/facets/master/facets-dist/facets-jupyter.html">
         <facets-dive id="elem" height="600"></facets-dive>
@@ -10,9 +11,8 @@ HTML_TEMPLATE = """
           var data = {jsonstr};
           document.querySelector("#elem").data = data;
         </script>"""
-
-def facets_display(data):
   jsonstr = data.to_json(orient='records')
   html = HTML_TEMPLATE.format(jsonstr=jsonstr)
+  print('Displaying factes plots inside Notebook. You may need different browser to view it corectly')
   display(HTML(html))
   
